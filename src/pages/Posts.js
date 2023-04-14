748c3dfa352ba15d0f60f1098d37aaa7f32aacc0
sohil <chavdamsohil@gmail.com>
2023-04-14 11:20:26 +0530

    windows

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -9,10 +9,8 @@ const Posts = ({ posts, loading }) => {
   return (
     <ul>
       {posts.map((post) => (
-        <div className={styles.container}>
-          <li key={post.id} className={styles.lists}>
-            {post.title}
-          </li>
+        <div key={post.id} className={styles.container}>
+          <li className={styles.lists}>{post.title}</li>
         </div>
       ))}
     </ul>
